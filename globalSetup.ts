46f8711bcd265ad96a86d6c06e3453016c9897cf
himakarkonanki <konankihimakar699@gmail.com>
2026-03-30 02:11:14 +0530

    moved the credentials to dotenv file

--- globalSetup.ts
+++ globalSetup.ts
@@ -1,15 +1,17 @@
 import { chromium } from "@playwright/test";
 import { testConfiguration } from "./test.config";
 import { expect } from "./src/fixtures/baseTest";
+import dotenv from "dotenv"
 
 async function globalSetup() {
+    dotenv.config()
     const browser = await chromium.launch({ headless: false })
     const context = await browser.newContext()
     const page = await context.newPage()
     await page.goto(testConfiguration.baseURL)
     await expect(page.locator('h1')).toHaveText('Sign in to EventHub')
-    await page.locator('#email').pressSequentially('test0609@gmail.com')
-    await page.locator('#password').pressSequentially('Aeiouvowels@6')
+    await page.locator('#email').pressSequentially(process.env.EMAIL!)
+    await page.locator('#password').pressSequentially(process.env.PASSWORD!)
     await page.getByRole('button', { name: 'Sign In' }).click()
     await page.waitForLoadState('networkidle');
     await context.storageState({ path: 'storageState.json' });
